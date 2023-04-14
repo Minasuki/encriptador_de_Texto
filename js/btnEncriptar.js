c0cebeb6fa1cc0ff39a19b41ce876afadcf30a25
Minasuki <jossiel2023@gmail.com>
2023-04-13 21:18:11 -0700

    creacion de los btn encrip, descrip y copi

---
+++ js/btnEncriptar.js
@@ -0,0 +1,17 @@
+(function () {
+    const ejecutaEncriptado = document.querySelector('.btn__Encriptar');
+    const ejecutaDesencriptado = document.querySelector('.btn__Desencriptar');
+    const ejecutaCopiar = document.querySelector('.btn__Copiar');
+
+    ejecutaEncriptado.addEventListener('click', () => {
+        alert('hola');
+    });
+
+    ejecutaDesencriptado.addEventListener('click', () => {
+        alert('hola');
+    });
+
+    ejecutaCopiar.addEventListener('click', () => {
+        alert('hola');
+    });
+})();
