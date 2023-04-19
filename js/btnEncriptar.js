8082712553a720dc1ad8ee08bdf61420481fa095
Minasuki <jossiel2023@gmail.com>
2023-04-19 00:35:40 -0700

    creada la funcion para encriptar y desencriptar

--- js/btnEncriptar.js
+++ js/btnEncriptar.js
@@ -4,104 +4,101 @@
     const copiar = document.querySelector('.btn__Copiar');
 
     encriptado.addEventListener('click', () => {
-        ajaxInfo;
+        ajaxInfo(true);
     });
 
     desencriptado.addEventListener('click', () => {
-        alert('hola');
+        ajaxInfo(false);
     });
 
     copiar.addEventListener('click', () => {
-        alert('hola');
+        copiarAlPortapapeles;
     });
 })();
 
-function ajaxInfo() {
-    var resultado = document.getElementById("informacion");
-
-    var xmlhttp;
+function ajaxInfo(boleano) {
+    let resultado = document.getElementById("info");
+    let xmlhttp;
 
     if (window.XMLHttpRequest) {
-
         xmlhttp = new XMLHttpRequest();
-
     } else {
-
         xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-
     }
 
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            /////////////////////////////////////////////////////////////////////////
+            let datosDelTextarea = document.getElementById("tex").value;
+            const arr = datosDelTextarea.split('');
+            switch (boleano) {
+                case true:
+                    for (let index = 0; index < arr.length; index++) {
+                        if (arr[index] === 'a') {
+                            arr[index] = 'ai';
+                        }
+                        if (arr[index] === 'e') {
+                            arr[index] = 'enter';
+                        }
+                        if (arr[index] === 'i') {
+                            arr[index] = 'imes';
+                        }
+                        if (arr[index] === 'o') {
+                            arr[index] = 'ober';
+                        }
+                        if (arr[index] === 'u') {
+                            arr[index] = 'ufat';
+                        }
+                    }
+                    resultado.innerHTML = '';
+                    resultado.innerHTML = arr.join('');
+                    break;
 
-            var datos = JSON.parse(xmlhttp.responseText);
+                case false:
+                    for (let index = 0; index < arr.length; index++) {
+                        if (arr[index] === 'a' && arr[index + 1] === 'i') {
+                            delete (arr[index + 1]);
 
-            console.log(datos);
-
-            if (resultado.innerHTML == "") {
-
-                for (var i in datos) {
-
-                    resultado.innerHTML += i + ": " + datos[i] + "<br/>";
-
-                }
-
-            }
+                        }
+                        if (arr[index] === 'e' && arr[index + 1] === 'n' && arr[index + 2] === 't'
+                            && arr[index + 3] === 'e' && arr[index + 4] === 'r') {
+                            delete (arr[index + 1]);
+                            delete (arr[index + 2]);
+                            delete (arr[index + 3]);
+                            delete (arr[index + 4]);
+                        }
+                        if (arr[index] === 'i' && arr[index + 1] === 'm' && arr[index + 2] === 'e'
+                            && arr[index + 3] === 's') {
+                            delete (arr[index + 1]);
+                            delete (arr[index + 2]);
+                            delete (arr[index + 3]);
+                        }
+                        if (arr[index] === 'o' && arr[index + 1] === 'b' && arr[index + 2] === 'e'
+                            && arr[index + 3] === 'r') {
+                            delete (arr[index + 1]);
+                            delete (arr[index + 2]);
+                            delete (arr[index + 3]);
+                        }
+                        if (arr[index] === 'u' && arr[index + 1] === 'f' && arr[index + 2] === 'a'
+                            && arr[index + 3] === 't') {
+                            delete (arr[index + 1]);
+                            delete (arr[index + 2]);
+                            delete (arr[index + 3]);
+                        }
+                    }
+                    resultado.innerHTML = '';
+                    resultado.innerHTML = arr.join('');
+                    break;
+            }////////////////////////////////////////////////////////////////////////
 
         }
     }
     xmlhttp.open("GET", "", true);
-
     xmlhttp.send();
-
 }
 
-/*
-function ajax_get_json() {
+function copiarAlPortapapeles() {
 
-    var resultado = document.getElementById("info");El id info se lo asignamos a la variable resultado
-
-    var xmlhttp;
-
-    if (window.XMLHttpRequest) {/*Cuando es los nagevadores modernos(google chrome)
-
-        xmlhttp = new XMLHttpRequest();
-
-    } else {Si son navegadores antiguos explore 5
-
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-
-    }
-
-
-    xmlhttp.onreadystatechange = function () {
-
-
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-
-           
-
-            var datos = JSON.parse(xmlhttp.responseText);
-
-           
-
-            console.log(datos);/*Imprimir en consola
-
-            if (resultado.innerHTML == "") {
-
-                for (var i in datos) {
-
-                    resultado.innerHTML += i + ": " + datos[i] + "<br/>";
-
-                }
-
-            }
-
-        }
-    }
-    xmlhttp.open("GET", "datos.json", true);
-
-    xmlhttp.send();
-
+    var text = document.getElementById("info").value;
+    clipboard.writeText(text);
 }
-*/
