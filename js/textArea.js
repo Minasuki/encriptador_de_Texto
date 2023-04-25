f1293e010e78e641dee1aa086fb24e30c23a47ad
Minasuki <jossiel2023@gmail.com>
2023-04-24 23:28:00 -0700

    intento d cambiarle dinamicamente el tamaño al are

---
+++ js/textArea.js
@@ -0,0 +1,13 @@
+(function () {
+const parrafo = document.querySelector('.parrafo');
+
+textarea.addEventListener('keydown', autosize);
+             
+function autosize(){
+  var el = this;
+  setTimeout(function(){
+    el.style.cssText = 'height:auto; padding:0';
+    el.style.cssText = 'height:' + el.scrollHeight + 'px';
+  },0);
+}
+})();
