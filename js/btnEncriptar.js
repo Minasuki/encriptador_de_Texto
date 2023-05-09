f6dcb8faf6d5e0935f9a8307fbc7fc0cfc0fc945
Minasuki <jossiel2023@gmail.com>
2023-05-08 23:07:50 -0700

    darle estilo a las alertas

--- js/btnEncriptar.js
+++ js/btnEncriptar.js
@@ -43,6 +43,12 @@ function ajaxInfo(boleano) {
 function copiarAlPortapapeles() {
     let text = document.getElementById("info");
     navigator.clipboard.writeText(text.textContent);
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Something went wrong!',
+        footer: '<a href="">Why do I have this issue?</a>'
+      })
 }
 
 function encriptar() {
